96a386cffedf22117920e864426c837d258caaef
Vinh <vinhbuith32412000@gmail.com>
2022-08-29 12:33:41 +0700

    Add Tag model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_26_073959) do
+ActiveRecord::Schema.define(version: 2022_08_29_050518) do
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.string "icon_path"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+tag_icon_paths = Dir["app/assets/images/tag/*"]
+tag_icon_paths.map do |path|
+  icon_file = path.split("/")[-1]
+  Tag.create(
+    name: icon_file.split(".")[0].split("_").join(" ").capitalize,
+    icon_path: icon_file
+  )
+end
