ee1362361ecf892c900256ebf14219a581df140c
Vinh <vinhbuith32412000@gmail.com>
2022-08-30 12:05:51 +0700

    Init static host/home controller and add link from user page to host page

---
+++ app/controllers/host/home_controller.rb
@@ -0,0 +1,5 @@
+class Host::HomeController < ApplicationController
+  def index 
+    render "host/home/index"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root "home#index"
+
+  namespace :host do 
+    resources :home
+  end
 end
