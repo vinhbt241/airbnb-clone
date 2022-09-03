e07ec7f75af73cd0ce8c7269dd0b9e5d9294352d
Vinh <vinhbuith32412000@gmail.com>
2022-09-03 20:53:09 +0700

    Refactor namespace host to Owner

---
+++ app/controllers/owner/home_controller.rb
@@ -0,0 +1,4 @@
+class Owner::HomeController < ApplicationController
+  def index 
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "home#index"
 
-  namespace :host do 
+  namespace :owner do 
     resources :home, only: %i[index]
   end
 end
