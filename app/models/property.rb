e162a0f48122924131eafa9bcb9f1fb883285d8b
Vinh <vinhbuith32412000@gmail.com>
2022-09-05 12:17:23 +0700

    Add images for property using Active storage, also generate images for test

--- app/models/property.rb
+++ app/models/property.rb
@@ -9,8 +9,14 @@ class Property < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: ->{ latitude.blank? && longitude.blank? }
 
+  has_many_attached :images, dependent: :destroy
+
   def address
     # [street, city, state, country].compact.join(', ')
     country
+  end
+
+  def default_image 
+    images.first
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,9 @@ tag_icon_paths.map do |path|
   )
 end
 
-20.times do 
+10.times do |i|
   property_name = Faker::Address.street_name
-  Property.create(
+  current_property = Property.create(
     name: property_name, 
     headline: property_name, 
     description: Faker::Lorem.paragraph,
@@ -20,4 +20,24 @@ end
     state: Faker::Address.state,
     country: Faker::Address.country
   )
+
+  current_property.images.attach(
+    io: File.open(Rails.root.join("app/assets/images/properties", "p_#{i + 1}.webp")), 
+    filename: current_property.name, 
+    content_type: "image/webp"
+  )
 end
+
+admin_1 = User.create(
+  email: "exampleadmin@gmail.com",
+  password: "admin123",
+  password_confirmation: "admin123"
+)
+
+admin_1.add_role :admin
+
+user_1 = User.create(
+  email: "exampleuser@gmail.com",
+  password: "user123",
+  password_confirmation: "user123"
+)
