e9e6474cadb49c2785393108106e62d9e5cab7e8
Vinh <vinhbuith32412000@gmail.com>
2022-09-07 14:46:00 +0700

    Remove redundant code

--- app/controllers/owner/home_controller.rb
+++ app/controllers/owner/home_controller.rb
@@ -5,12 +5,8 @@ class Owner::HomeController < ApplicationController
   end
 
   def start_build_property 
-    if current_user
-      @property = current_user.properties.build(status: "")
-      @property.save
-      redirect_to owner_property_build_property_index_path(property_id: @property.id)
-    else
-      redirect_to new_user_session_path
-    end
+    @property = current_user.properties.build(status: "")
+    @property.save
+    redirect_to owner_property_build_property_index_path(property_id: @property.id)
   end
 end
