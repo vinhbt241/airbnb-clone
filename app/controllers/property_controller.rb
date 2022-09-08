52107c71681ee822380b156a601781181247c6cc
Vinh <vinhbuith32412000@gmail.com>
2022-09-08 16:41:32 +0700

    Config datepicker controller to disable rented dates from calendar

--- app/controllers/property_controller.rb
+++ app/controllers/property_controller.rb
@@ -1,5 +1,11 @@
 class PropertyController < ApplicationController
   def show 
     @property = Property.find(params[:id])
+
+    @date_ranges = []
+    @property.reservations.each do |reservation|
+      @date_ranges << ["#{reservation.from}", "#{reservation.to}"]
+    end
+
   end
 end
--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -3,11 +3,24 @@ import flatpickr from "flatpickr"
 import 'flatpickr/dist/flatpickr.min.css'
 
 export default class extends Controller {
+  static values = {
+    disablerange: Array
+  }
+
   connect() {
+    let disableHash = []
+    this.disablerangeValue.forEach(element => {
+      disableHash.push({
+        from: element[0],
+        to: element[1]
+      })
+    })
+
     flatpickr(".datepicker", {
       minDate: "today",
       showMonths: 2,
-      allowInput: true
+      allowInput: true,
+      disable: disableHash
     });
   }
 }
