6d4db5e27fe2ea88a1dd3ccc3362f277a822173a
Vinh <vinhbuith32412000@gmail.com>
2022-09-12 14:43:53 +0700

    Complete payment process

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -16,22 +16,22 @@ class ReservationsController < ApplicationController
     Property.find(params[:property_id]).reservations.each do |reservation|
       @date_ranges << ["#{reservation.from}", "#{reservation.to}"]
     end
-
-    current_user.set_payment_processor :stripe
-    current_user.payment_processor.customer 
-
-    @checkout_session = current_user.payment_processor.checkout(
-      mode: "payment",
-      line_items: "#{Property.find(params[:property_id]).price_id}",
-      success_url: "http://localhost:3000#{reservations_path}"
-    )
   end
 
   def create 
     @reservation = Reservation.new(reservation_params)
 
     if @reservation.save 
-      redirect_to reservations_path
+      @property = Property.find(params[:property_id])
+      current_user.set_payment_processor :stripe
+      current_user.payment_processor.customer 
+
+      @checkout_session = current_user.payment_processor.checkout(
+        mode: "payment",
+        line_items: "#{@property.price_id}",
+        success_url: "http://localhost:3000#{reservations_path}",
+        cancel_url: "http://localhost:3000#{property_path(@property)}"
+      )
     else
       render :new, status: :unprocessable_entity
     end
