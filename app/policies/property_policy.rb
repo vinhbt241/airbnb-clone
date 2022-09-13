2b5e7b64b7d09d296730107e3de7a559d9d4b5bf
Vinh <vinhbuith32412000@gmail.com>
2022-09-13 11:49:24 +0700

    Refactor update images

--- app/policies/property_policy.rb
+++ app/policies/property_policy.rb
@@ -1,9 +1,9 @@
 class PropertyPolicy < ApplicationPolicy
   def show? 
-    user.has_role?(:admin) || record.owner.id == current_user.id 
+    user.has_role?(:admin) || record.owner.id == user.id 
   end
 
   def update? 
-    user.has_role?(:admin) || record.owner.id == current_user.id 
+    user.has_role?(:admin) || record.owner.id == user.id 
   end
 end
