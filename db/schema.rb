bbf4924c035932fe8171e25c425a775fd7cb63e0
Vinh <vinhbuith32412000@gmail.com>
2022-09-14 10:23:26 +0700

    Add user confirmation step with devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_13_085523) do
+ActiveRecord::Schema.define(version: 2022_09_14_022339) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -181,6 +181,10 @@ ActiveRecord::Schema.define(version: 2022_09_13_085523) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
